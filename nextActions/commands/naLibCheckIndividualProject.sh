1bf0dc0892fb7bdff815175592fa00629155ec12
Joseph Reddington <joe@joereddington.com>
2014-12-17 08:58:51 +0000

    Update to hardcoded paths

--- nextActions/commands/naLibCheckIndividualProject.sh
+++ nextActions/commands/naLibCheckIndividualProject.sh
@@ -1,6 +1,6 @@
 echo ", $1"
-if grep --quiet ", $1$" /home/joereddington/Jurgen/nextActions/nextActions.csv; then
+if grep --quiet ", $1$" /home/joereddington/joereddington.com/Jurgen/nextActions/nextActions.csv; then
   echo updated NOT needed
 else
-  /home/joereddington/Jurgen/nextActions/commands/naFulltdcp "Update Projects, $1" 2 g 3
+  /home/joereddington/joereddington.com/Jurgen/nextActions/commands/naFulltdcp "Update Projects, $1" 2 g 3
 fi
--- nextActions/commands/naLibSorting.py
+++ nextActions/commands/naLibSorting.py
@@ -8,7 +8,7 @@ import os
 
 
 def sortTasksCSV():
-   with open(os.environ['JURGEN']+'/Jurgen/nextActions/nextActions.csv', 'rb') as f:
+   with open(os.environ['JURGEN']+'/joereddington.com/Jurgen/nextActions/nextActions.csv', 'rb') as f:
 	reader = csv.reader(f, skipinitialspace=True)
 	tasklist=sorted(reader,key=operator.itemgetter(3), reverse=True)
 	tasklist.sort(key=lambda item: datetime.datetime.strptime(item[3], '%Y-%m-%d (%a) -  %H:%M:%S'))
